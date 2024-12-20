d9ddd0b0b78ef92e4482c71e803e592f71ee3255
sahil9396 <sahilarchansreekanth@gmail.com>
2024-12-21 01:46:28 +0530

    Image Problem is Solved

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,3 @@
-import maa from "@/public/maa.jpg";
 import Image from "next/image";
 
 export default function Sidebar() {
@@ -8,10 +7,10 @@ export default function Sidebar() {
         <h3 className="text-xl font-serif font-bold mb-4">About Me</h3>
         <div className="aspect-square w-32 mx-auto mb-4 overflow-hidden rounded-full">
           <Image
-            src={maa.src}
+            src={process.env.ADMIN_IMAGE_URL || ""}
             alt="Author"
-            width={128}
-            height={128}
+            width={1024}
+            height={1024}
             className="object-cover object-top w-32 h-32"
           />
         </div>
