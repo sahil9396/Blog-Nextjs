5164206b1f0bf92d049492fb55c5c61bb451857f
sahil9396 <sahilarchansreekanth@gmail.com>
2024-12-23 15:08:45 +0530

    installed analytics cammonds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Merriweather } from "next/font/google";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
